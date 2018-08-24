2279d3f1954c5475f54eeced1f06c711983fcf1a
Juan Leon <jlp25@hotmail.com>
2018-08-23 19:48:08 -0500

    Elimina sesion usuario

--- config/autoload.php
+++ config/autoload.php
@@ -11,31 +11,6 @@
 	Database::config_conection_db(nombre_servidor,usuario,clave,base_datos);
 
 	
-
-	/*=================================================*/
-
-	/*=================================================*/
-	// Autologin
-		// session_start();
-		$log_in = Cookie::readCookie('lg_us_rem');
-
-		if ($log_in AND isset($log_in['close']) AND !$log_in['close']) {
-			$where = 'correo = ? AND clave = ? AND tm_delete IS NULL';
-			$params = ['ss',$log_in['correo'],Secure::montar_clave_verificacion($log_in['clave'])];
-			$user_info = CRUD::all('usuarios','*',$where,$params);
-			if (count($user_info) > 0) {
-				$_SESSION['user'] = Sqlconsult::escape($user_info[0]['nombre']);
-				$_SESSION['id_usuario'] = $user_info[0]['id_usuario'];
-			  	$_SESSION['csrf_token'] = parent::crear_csrf_token();
-			 	$_SESSION['csrf_token_time'] = time();
-			}
-		}	
-
-		$session = false;
-		if (isset($_SESSION['id_usuario'])) {
-			$session = true;
-		}
-
 	/*=================================================*/
 
 	/*=============================================*/
